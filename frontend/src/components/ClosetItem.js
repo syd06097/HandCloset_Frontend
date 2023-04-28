3a1ff6d39cb8e19c4991ace9d5835f941513fa3a
chgim <chgim1128@gmail.com>
2023-04-28 20:32:20 +0900

    chanho
    update

--- frontend/src/components/ClosetItem.js
+++ frontend/src/components/ClosetItem.js
@@ -1,9 +1,22 @@
 import React from "react";
+import PlusIcon from "../images/PlusIcon.png";
+import { useNavigate } from "react-router-dom";
 
 const ClosetItem = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>ClosetItem</h1>
+      <h3>등록한 옷이 없어요.</h3>
+      <div className="Plus">
+        <img
+          src={PlusIcon}
+          alt="추가하기"
+          style={{ width: "15%" }}
+          onClick={() => {
+            navigate("/calendar");
+          }}
+        />
+      </div>
     </div>
   );
 };
